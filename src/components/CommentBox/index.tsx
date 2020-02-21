bd74266cdc811244965feec5e1bdb9f690b42be7
WebDeveloperReact <happywithyouweb@gmail.com>
2020-02-21 04:16:54 -0500

    trying to fix commentitem

--- src/components/CommentBox/index.tsx
+++ src/components/CommentBox/index.tsx
@@ -2,7 +2,7 @@ import React, { useRef, useState } from 'react';
 import {
   StyleSheet,
   View,
-  FlatList,
+  Image,
   TouchableOpacity,
   Text,
   TextInput
@@ -23,8 +23,8 @@ const CommentBox = (props: CommentBoxProps) => {
 
   const offsetY = new Animated.Value(0);
   const commentListRef = useRef({} as any);
-
   const [comment, setComment] = useState('');
+  const [isCommentBoxVisable, setIsCommentBoxVisable] = useState(false);
 
   const handleChangeComment = (newComment: string) => {
     setComment(newComment);
@@ -35,36 +35,63 @@ const CommentBox = (props: CommentBoxProps) => {
       return;
     }
     addVideoComment(vimeoId, comment);
+    if (!isCommentBoxVisable) {
+      setIsCommentBoxVisable(true);
+    }
+    setComment('');
   };
 
+  const handleChangeCommentBoxVisable = () => {
+    setIsCommentBoxVisable(visable => !visable);
+  };
+
+  if (!videoComments) return null;
+
   return (
-    <View style={styles.container}>
-      <View style={styles.commentListBox}>
-        <Animated.ScrollView
-          onScroll={Animated.event(
-            [{ nativeEvent: { contentOffset: { y: offsetY } } }],
-            { useNativeDriver: true }
-          )}
-          scrollEventThrottle={16}
-          ref={commentListRef}
-        >
-          {videoComments.map((comment, index) => (
-            <CommentItem comment={comment} />
-          ))}
-        </Animated.ScrollView>
-      </View>
+    <View
+      style={{ ...styles.container, height: isCommentBoxVisable ? 200 : 51 }}
+    >
+      {isCommentBoxVisable && (
+        <View style={styles.commentListBox}>
+          <Animated.ScrollView
+            onScroll={Animated.event(
+              [{ nativeEvent: { contentOffset: { y: offsetY } } }],
+              { useNativeDriver: true }
+            )}
+            scrollEventThrottle={16}
+            ref={commentListRef}
+          >
+            {videoComments.map((comment, index) => (
+              <CommentItem comment={comment} key={'comment' + index} />
+            ))}
+          </Animated.ScrollView>
+        </View>
+      )}
       <View style={styles.commentInputContainer}>
+        <TouchableOpacity
+          style={styles.commentsButton}
+          onPress={handleChangeCommentBoxVisable}
+        >
+          <Image
+            style={styles.commentsButtonIcon}
+            source={require('../../../assets/comments.png')}
+          />
+        </TouchableOpacity>
         <TextInput
           style={styles.commentInput}
           value={comment}
           onEndEditing={handlePostComment}
           onChangeText={handleChangeComment}
+          placeholder="Comment"
         />
         <TouchableOpacity
           style={styles.commentSendButton}
           onPress={handlePostComment}
         >
-          <Text style={styles.commentSendButtonIcon}>{'Send'}</Text>
+          <Image
+            style={styles.commentSendButtonIcon}
+            source={require('../../../assets/sendbutton.png')}
+          />
         </TouchableOpacity>
       </View>
     </View>
@@ -73,40 +100,48 @@ const CommentBox = (props: CommentBoxProps) => {
 
 const styles = StyleSheet.create({
   container: {
-    height: '100%',
+    position: 'absolute',
+    bottom: 0,
+    height: 200,
+    zIndex: 2,
     width: '100%'
   },
   commentListBox: {
     flex: 1,
-    marginBottom: 40
+    marginBottom: 50
   },
   commentInputContainer: {
     position: 'absolute',
-    bottom: 0,
-    height: 40,
-    width: '100%'
+    bottom: 6,
+    width: '100%',
+    height: 45,
+    zIndex: 3,
+    flexDirection: 'row'
   },
   commentInput: {
     flex: 1,
-    height: 40,
-    backgroundColor: 'rgba(0, 0, 0, 0.1)',
-    borderRadius: 5,
-    paddingLeft: 10,
-    paddingRight: 10,
-    fontSize: 14
+    backgroundColor: 'rgba(0, 0, 0, 0.2)',
+    paddingHorizontal: 10,
+    fontSize: 16
   },
   commentSendButton: {
-    width: 80,
-    height: 40,
-    marginLeft: 10,
+    width: 50,
     alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: 'rgb(33, 150, 243)'
+    justifyContent: 'center'
   },
   commentSendButtonIcon: {
-    fontSize: 16,
-    fontWeight: '700',
-    color: 'white'
+    width: 45,
+    height: 45
+  },
+  commentsButton: {
+    width: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0)'
+  },
+  commentsButtonIcon: {
+    width: 45,
+    height: 45
   }
 });
 
